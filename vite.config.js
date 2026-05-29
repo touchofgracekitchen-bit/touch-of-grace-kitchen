dab2ca7906493b7d691747799c6c995b1cce630f
touchofgracekitchen-bit <touchofgracekitchen@gmail.com>
2026-05-29 08:41:01 -0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    target: 'es2015',
+    chunkSizeWarningLimit: 1000,
+  },
+  esbuild: {
+    target: 'es2015'
+  }
 })
